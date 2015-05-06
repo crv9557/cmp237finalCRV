bb2a1d68ea967fc50204697fa514de0ff4baac91
Connor Ring <connorring7778@sjccs142.saintjoe.edu>
2015-05-06 09:51:27 -0400

    Finished problem 0

---
+++ scope.js
@@ -0,0 +1,17 @@
+// Connor Ring Scope Question
+// CMP237
+
+// Create the function with hidden variables
+function foo() {
+  var x = 5;
+  var y = 10;
+}
+
+//Make dummy variables
+var x = 1
+var y = 2
+
+//Print the variables
+console.log(x)
+console.log(y)
+
